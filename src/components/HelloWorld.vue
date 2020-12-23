0737e6b679145beb4a99ac54a06ae356c02194fb
wangcong <1151651564@qq.com>
2020-12-23 23:55:45 +0800

    mock data with mockjs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,27 @@
 <template>
+  <!-- eslint-disable -->
   <div class="hello">
     <h1>{{ msg }}</h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Mock from 'mockjs';
+
+Mock.mock('/mockjs/users', 'get', {
+  'users|1-10': [
+    Mock.Random.first(),
+  ]
+});
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
   },
   mounted() {
-    fetch('/mock/users')
+    axios.get('/mockjs/users')
       .then(console.log);
   }
 }
